32d17a9cd16d4f214bb2afe412083c921232acb7
dadadon <davianberoni@yahoo.com>
2023-08-04 10:30:31 -0500

    Set up linter

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem "rails", "~> 7.0.6"
 
 gem 'jsbundling-rails'
 
+gem 'rubocop', '>= 1.0', '< 2.0'
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,21 @@
-// const path    = require("path")
-// const webpack = require("webpack")
-
-// module.exports = {
-//   mode: "production",
-//   devtool: "source-map",
-//   entry: {
-//     application: "./app/javascript/application.js"
-//   },
-//   output: {
-//     filename: "[name].js",
-//     sourceMapFilename: "[file].map",
-//     path: path.resolve(__dirname, "app/assets/builds"),
-//   },
-//   plugins: [
-//     new webpack.optimize.LimitChunkCountPlugin({
-//       maxChunks: 1
-//     })
-//   ]
-// }
 const { webpackConfig } = require('@rails/webpacker');
-module.exports = webpackConfig;
 
+module.exports = {
+  ...webpackConfig,
+  module: {
+    rules: [
+      // Add your custom rules here
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      // Add other rules as needed for different file types
+    ],
+  },
+};
