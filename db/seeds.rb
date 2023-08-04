638c490f2f39e8afa308bf0fa550cca9aef66f60
dadadon <davianberoni@yahoo.com>
2023-08-04 09:35:32 -0500

    Add greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: 'Hello World!')
+Greeting.create(message: 'Hi there!')
+Greeting.create(message: 'Welcome!')
+Greeting.create(message: 'Greetings!')
+Greeting.create(message: 'Hola!')
