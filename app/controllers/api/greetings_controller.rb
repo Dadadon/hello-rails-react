025f5c56f82550f96b5b0d35b079c88ea6868df2
dadadon <davianberoni@yahoo.com>
2023-08-04 10:36:58 -0500

    Fix linter errors

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,9 @@
-class Api::GreetingsController < ApplicationController
+# frozen_string_literal: true
+module Api
+  class GreetingsController < ApplicationController
     def random
       @greeting = Greeting.order('RANDOM()').first
       render json: { message: @greeting.message }
     end
-  end  
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,11 +12,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_04_142328) do
-  create_table "greetings", force: :cascade do |t|
-    t.string "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.0].define(version: 20_230_804_142_328) do
+  create_table 'greetings', force: :cascade do |t|
+    t.string 'message'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
